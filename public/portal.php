5248902cc02624ff53f39792abecb2f65d797646
Joseph Tresca <jtresca@glgroup.com>
2014-11-17 13:39:08 -0500

    adding in more bits to see what breaks

--- public/portal.php
+++ public/portal.php
@@ -288,6 +288,47 @@ if (($uname != null && $uname != "error") && ($jsonApps["apps"] != null)) {
 	}
 }
 
+//BEGIN RANDOM BACKGROUND
+
+$val = null;
+
+function displayBackground() {
+	$dir     = 'img/bg/';
+	$cnt     = 0;
+	$bgArray = array();
+
+	/*if we can load the directory*/
+	if ($handle = opendir($dir)) {
+
+		/* Loop through the directory here */
+		while (false !== ($entry = readdir($handle))) {
+
+			$pathToFile = $dir.$entry;
+			if (is_file($pathToFile))//if the files exists
+			{
+
+				//make sure the file is an image...there might be a better way to do this
+				if (getimagesize($pathToFile) != FALSE) {
+					//add it to the array
+					$bgArray[$cnt] = $pathToFile;
+					$cnt           = $cnt+1;
+
+				}
+
+			}
+
+		}
+		//create a random number, then use the image whos key matches the number
+		$myRand = rand(0, ($cnt-1));
+		$val    = $bgArray[$myRand];
+
+	}
+	closedir($handle);
+	echo ('"'.$val.'"');
+
+}
+// END RANDOM BACKGROUND
+
 // Add remaining Singlepoint apps
 if ($spApps != null) {
 	for ($i = 0; $i < sizeof($spApps); $i++) {
