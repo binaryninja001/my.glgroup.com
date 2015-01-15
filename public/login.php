ea686fc69c22a43c307a5bff635d69c3c68ef3a3
Duane Fields <duane@duanefields.com>
2015-01-15 11:42:09 -0600

    hide this page from the google index

--- public/login.php
+++ public/login.php
@@ -9,6 +9,7 @@
 <head>
   <title>MyGLG Login</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+  <meta name="robots" content="noindex">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
   <link rel="stylesheet" type="text/css" href="css/main.css">
